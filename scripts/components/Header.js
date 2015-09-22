3eabdfe5d2515d9c4f74f12d887d5634555553e6
Andrew <andrew.n@socialcodeinc.com>
2015-09-22 15:28:31 -0700

    adding logo

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -5,6 +5,9 @@ class Header extends Component {
         return (
             <div className='header'>
                 <div className='container'>
+                    <div className='header-logo'>
+                        <i className='icon ion-radio-waves' />
+                    </div>
                     <ul className='header-nav'>
                         <li className='header-nav-item'>
                             <a className='header-nav-item-link active'>SoundRedux</a>
