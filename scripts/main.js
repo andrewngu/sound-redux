6fa5e7f5d7e81b978e3b1fab5cf087fc5df8b7a9
Andrew Nguyen <andrewngu@gmail.com>
2015-09-28 05:30:17 -0700

    moving app to containers

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import App from './components/App';
+import App from './containers/App';
 import configureStore from './store/configureStore';
 
 require('../sass/main.scss');
