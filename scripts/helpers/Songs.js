adc61820ec244b3876b70420075dfc38b7cfa186
Andrew Nguyen <andrewngu@gmail.com>
2015-10-01 10:36:48 -0700

    fixing constructUrl

--- scripts/helpers/Songs.js
+++ scripts/helpers/Songs.js
@@ -2,14 +2,14 @@ import {CLIENT_ID} from '../constants/Config';
 import {GENRES_MAP} from '../constants/Genres';
 
 export function constructUrl(category) {
-    if (category !== 'house'
-    && category !== 'trance'
-    && category !== 'dubstep') {
-        category = `${category} house`;
-    }
-
     let result = `http://api.soundcloud.com/tracks?linked_partitioning=1&client_id=${CLIENT_ID}&limit=50&offset=0`;
     if (category in GENRES_MAP) {
+        if (category !== 'house'
+        && category !== 'trance'
+        && category !== 'dubstep') {
+            category = `${category} house`;
+        }
+        
         result += `&tags=${category}`;
     } else {
         result += `&q=${category}`;
