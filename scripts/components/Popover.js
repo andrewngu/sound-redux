2132192798e69129de0466bef742bb43d7f55857
Andrew Nguyen <andrewngu@gmail.com>
2015-10-02 17:03:27 -0700

    adding outside click handler to popover

--- scripts/components/Popover.js
+++ scripts/components/Popover.js
@@ -3,8 +3,43 @@ import React, {Component, PropTypes} from 'react';
 class Popover extends Component {
     constructor(props) {
         super(props);
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+        this.onOutsideClick = this.onOutsideClick.bind(this);
         this.toggleIsOpen = this.toggleIsOpen.bind(this);
+
         this.state = {isOpen: false};
+    }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.onOutsideClick);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.onOutsideClick);
+    }
+
+    handleOutsideClick() {
+        this.setState({
+            isOpen: false
+        });
+    }
+
+    onOutsideClick(e) {
+        if (!this.state.isOpen) {
+            return;
+        }
+
+        e.stopPropagation();
+        const localNode = React.findDOMNode(this);
+        let source = e.target;
+
+        while(source.parentNode) {
+            if (source === localNode) {
+                return;
+            }
+            source = source.parentNode;
+        }
+        this.handleOutsideClick();
     }
 
     toggleIsOpen() {
