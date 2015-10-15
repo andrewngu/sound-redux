3ca2b4bec5da3726dfbd166cdd1db1f7ee5ccf7f
Andrew Nguyen <andrewngu@gmail.com>
2015-10-14 20:14:54 -0700

    making progress on user detail view, fixing history pushstate bug

--- scripts/actions/navigator.js
+++ scripts/actions/navigator.js
@@ -13,22 +13,23 @@ function changePath(path) {
 export function navigateBack(e) {
     return dispatch => {
         if (e.state) {
-            return dispatch(changePath(e.state.path));
+            return dispatch(navigateTo(e.state.path, false));
         }
     }
 }
 
-export function navigateTo(path) {
+export function navigateTo(path, shouldPushState = true) {
     return dispatch => {
         if (path[0] === 'songs' && path.length === 2) {
             dispatch(changeActiveSong(path[1]));
 
         } else if (path[0] === 'users' && path.length === 2) {
             dispatch(changeActiveUser(path[1]));
-
         }
 
-        pushState(path);
+        if (shouldPushState) {
+            pushState(path);
+        }
         return dispatch(changePath(path))
     }
 }
--- scripts/components/User.js
+++ scripts/components/User.js
@@ -2,9 +2,23 @@ import React, {Component, PropTypes} from 'react';
 import {getImageUrl} from '../helpers/SongsHelper';
 
 class User extends Component {
+    renderLocation() {
+        const {user} = this.props;
+
+        if (user.city && user.country) {
+            return `${user.city}, ${user.country}`;
+        } else if (user.city) {
+            return user.city;
+        } else if (user.country) {
+            return user.country;
+        }
+
+        return 'Earth';
+    }
+
     render() {
         const {user} = this.props;
-        const image = getImageUrl(user.avatar_url);
+        const image = user.avatar_url ? getImageUrl(user.avatar_url) : null;
         return (
             <div className='container'>
                 <div className='content'>
@@ -13,6 +27,15 @@ class User extends Component {
                             <div className='user card'>
                                 <div className='user-detail'>
                                     <img className='user-image' src={image} />
+                                </div>
+                                <div className='user-info'>
+                                    <div className='user-username'>{user.username}</div>
+                                    <div className='user-location'>
+                                        <i className='icon ion-location'></i>
+                                        {this.renderLocation()}
+                                    </div>
+                                    <div className='user-description' dangerouslySetInnerHTML={{__html: user.description}}>
+                                    </div>
                                 </div>
                             </div>
                         </div>
