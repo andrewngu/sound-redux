0c46bf0d0ca87d90d359656bc116bcb85c532100
Andrew Nguyen <andrewngu@gmail.com>
2015-10-18 20:02:37 -0700

    using go as app server

---
+++ server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.Handle("/", http.FileServer(http.Dir("public"))).Methods("GET")
+
+	n := negroni.Classic()
+	n.UseHandler(r)
+	n.Run(":8080")
+}
