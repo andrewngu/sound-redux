3aa136d100a94aa1f87a41d7b5e28046f205d76b
Andrew Nguyen <andrewngu@gmail.com>
2015-11-05 18:42:26 -0800

    fixing SongCard

--- scripts/components/SongCard.js
+++ scripts/components/SongCard.js
@@ -1,156 +1,61 @@
 import React, {Component, PropTypes} from 'react';
-import {playSong} from '../actions/player';
-import {fetchSongsIfNeeded} from '../actions/playlists';
-import InfiniteScrollify from '../components/InfiniteScrollify';
-import SongCard from '../components/SongCard';
-import Spinner from '../components/Spinner';
+import Link from '../components/Link';
+import SongDetails from '../components/SongDetails';
+import SongHeart from '../components/SongHeart';
+import {IMAGE_SIZES} from '../constants/SongConstants';
+import {formatSongTitle} from '../utils/FormatUtils';
+import {getImageUrl} from '../utils/SongUtils';
 
-class SongCards extends Component {
-    constructor(props) {
-        super(props);
-        this.getScrollState = this.getScrollState.bind(this);
-        this.onScroll = this.onScroll.bind(this);
-        const {playlist, playlists} = props;
-        const items = playlist in playlists ? playlists[playlist].items : [];
-        this.state = {
-            end: items.length,
-            paddingBottom: 0,
-            paddingTop: 0,
-            start: 0
-        };
-    }
-
-    componentDidMount() {
-        window.addEventListener('scroll', this.onScroll, false);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        const {end, paddingBottom, paddingTop, start} = this.getScrollState(nextProps);
-        if (paddingTop !== this.state.paddingTop
-        || paddingBottom !== this.state.paddingBottom
-        || start !== this.state.start
-        || end !== this.state.end) {
-            this.setState({
-                end: end,
-                paddingBottom: paddingBottom,
-                paddingTop: paddingTop,
-                start: start
-            });
-        }
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.onScroll, false);
-    }
-
-    getScrollState(props) {
-        const {height, playlists, playlist} = props;
-        const items = playlist in playlists ? playlists[playlist].items : [];
-
-        const MARGIN_TOP = 20;
-        const ROW_HEIGHT = 132;
-        const ITEMS_PER_ROW = 5;
-        const scrollY = window.scrollY;
-        let paddingTop = 0;
-        let paddingBottom = 0;
-        let start = 0;
-        let end = items.length;
-
-        if ((scrollY - ((ROW_HEIGHT * 3) + (MARGIN_TOP * 2))) > 0) {
-            const rowsToPad = Math.floor((scrollY - ((ROW_HEIGHT * 2) + (MARGIN_TOP))) / (ROW_HEIGHT + MARGIN_TOP));
-            paddingTop = rowsToPad * (ROW_HEIGHT + MARGIN_TOP);
-            start = rowsToPad * ITEMS_PER_ROW;
-        }
-
-        const rowsOnScreen = Math.ceil(height / (ROW_HEIGHT + MARGIN_TOP));
-        const itemsToShow = (rowsOnScreen + 5) * ITEMS_PER_ROW;
-        if (items.length > (start + itemsToShow)) {
-            end = start + itemsToShow;
-            const rowsToPad = Math.ceil((items.length - end) / ITEMS_PER_ROW);
-            paddingBottom = rowsToPad * (ROW_HEIGHT + MARGIN_TOP);
-        }
-
-        return {
-            end,
-            paddingBottom,
-            paddingTop,
-            start
-        };
-    }
-
-    onScroll() {
-        const {end, paddingBottom, paddingTop, start} = this.getScrollState(this.props);
-        if (paddingTop !== this.state.paddingTop
-        || paddingBottom !== this.state.paddingBottom
-        || end !== this.state.end
-        || start !== this.state.start) {
-            this.setState({
-                end: end,
-                paddingBottom: paddingBottom,
-                paddingTop: paddingTop,
-                start: start
-            });
-        }
-    }
-
-    playSong(i) {
-        const {playlist, dispatch} = this.props;
-        dispatch(playSong(playlist, i));
-    }
-
-    renderSongs(start, end) {
-        const chunk = 5;
-        const {authed, dispatch, playlist, playlists, playingSongId, songs, users} = this.props;
-        const items = playlist in playlists ? playlists[playlist].items : [];
-        let result = [];
-        for (let i = start; i < end; i += chunk) {
-            let songCards = items.slice(i, i + chunk).map((songId, j) => {
-                const song = songs[songId];
-                const user = users[song.user_id];
-                const index = i + j;
-                return (
-                    <div className='col-1-5' key={index + '-' + song.id}>
-                        <SongCard
-                            authed={authed}
-                            dispatch={dispatch}
-                            isActive={song.id === playingSongId}
-                            playSong={this.playSong.bind(this, index)}
-                            scrollFunc={fetchSongsIfNeeded.bind(null, playlist)}
-                            song={song}
-                            user={user} />
-                    </div>
-                );
-            }, this);
-
-            if (songCards.length < chunk) {
-                for (let j = 0; j < chunk - songCards.length + 1; j++) {
-                    songCards.push(<div className='col-1-5' key={'song-placeholder-' + (i + j)}></div>);
-                }
-            }
-
-            result.push(
-                <div className='songs-row grid' key={'songs-row-' + i}>{songCards}</div>
-            );
-        }
-
-        return result;
-    }
-
+class SongCard extends Component {
     render() {
-        const {playlist, playlists} = this.props;
-        const {end, paddingBottom, paddingTop, start} = this.state;
-        const isFetching = playlist in playlists ? playlists[playlist].isFetching : false;
-
+        const {authed, dispatch, isActive, playSong, song, user} = this.props;
+        const isLiked = song.id in authed.likes && authed.likes[song.id] == 1;
+        const image = getImageUrl(song.artwork_url, IMAGE_SIZES.LARGE);
 
         return (
-            <div className='content'>
-                <div className='padder' style={{height: paddingTop}}></div>
-                {this.renderSongs(start, end)}
-                <div className='padder' style={{height: paddingBottom}}></div>
-                {isFetching ? <Spinner /> : null}
+            <div className={'card song-card' + (isActive ? ' active' : '')}>
+                <div
+                    className='song-card-image'
+                    onClick={playSong}
+                    style={{backgroundImage: `url(${image})`}}>
+                    <div className='song-card-playing'>
+                        <i className={'song-card-playing-icon icon ' + (isActive ? 'ion-radio-waves' : 'ion-ios-play')}></i>
+                    </div>
+                </div>
+                <div className='song-card-user clearfix'>
+                    <img
+                        className='song-card-user-image'
+                        src={getImageUrl(user.avatar_url)} />
+                    <div className='song-card-details'>
+                        <Link
+                            className='song-card-title'
+                            dispatch={dispatch}
+                            route={{path: ['songs', song.id]}}>
+                            {formatSongTitle(song.title)}
+                        </Link>
+                        <Link
+                            className='song-card-user-username'
+                            dispatch={dispatch}
+                            route={{path: ['users', user.id]}}>
+                            {user.username}
+                        </Link>
+                        <SongHeart
+                            authed={authed}
+                            className='song-card-heart'
+                            dispatch={dispatch}
+                            isLiked={isLiked}
+                            songId={song.id} />
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default InfiniteScrollify(SongCards);
+SongCard.propTypes = {
+    isActive: PropTypes.bool.isRequired,
+    playSong: PropTypes.func.isRequired,
+    song: PropTypes.object.isRequired
+};
+
+export default SongCard;
---
+++ scripts/components/SongCards.js
@@ -0,0 +1,156 @@
+import React, {Component, PropTypes} from 'react';
+import {playSong} from '../actions/player';
+import {fetchSongsIfNeeded} from '../actions/playlists';
+import InfiniteScrollify from '../components/InfiniteScrollify';
+import SongCard from '../components/SongCard';
+import Spinner from '../components/Spinner';
+
+class SongCards extends Component {
+    constructor(props) {
+        super(props);
+        this.getScrollState = this.getScrollState.bind(this);
+        this.onScroll = this.onScroll.bind(this);
+        const {playlist, playlists} = props;
+        const items = playlist in playlists ? playlists[playlist].items : [];
+        this.state = {
+            end: items.length,
+            paddingBottom: 0,
+            paddingTop: 0,
+            start: 0
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll, false);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {end, paddingBottom, paddingTop, start} = this.getScrollState(nextProps);
+        if (paddingTop !== this.state.paddingTop
+        || paddingBottom !== this.state.paddingBottom
+        || start !== this.state.start
+        || end !== this.state.end) {
+            this.setState({
+                end: end,
+                paddingBottom: paddingBottom,
+                paddingTop: paddingTop,
+                start: start
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll, false);
+    }
+
+    getScrollState(props) {
+        const {height, playlists, playlist} = props;
+        const items = playlist in playlists ? playlists[playlist].items : [];
+
+        const MARGIN_TOP = 20;
+        const ROW_HEIGHT = 132;
+        const ITEMS_PER_ROW = 5;
+        const scrollY = window.scrollY;
+        let paddingTop = 0;
+        let paddingBottom = 0;
+        let start = 0;
+        let end = items.length;
+
+        if ((scrollY - ((ROW_HEIGHT * 3) + (MARGIN_TOP * 2))) > 0) {
+            const rowsToPad = Math.floor((scrollY - ((ROW_HEIGHT * 2) + (MARGIN_TOP))) / (ROW_HEIGHT + MARGIN_TOP));
+            paddingTop = rowsToPad * (ROW_HEIGHT + MARGIN_TOP);
+            start = rowsToPad * ITEMS_PER_ROW;
+        }
+
+        const rowsOnScreen = Math.ceil(height / (ROW_HEIGHT + MARGIN_TOP));
+        const itemsToShow = (rowsOnScreen + 5) * ITEMS_PER_ROW;
+        if (items.length > (start + itemsToShow)) {
+            end = start + itemsToShow;
+            const rowsToPad = Math.ceil((items.length - end) / ITEMS_PER_ROW);
+            paddingBottom = rowsToPad * (ROW_HEIGHT + MARGIN_TOP);
+        }
+
+        return {
+            end,
+            paddingBottom,
+            paddingTop,
+            start
+        };
+    }
+
+    onScroll() {
+        const {end, paddingBottom, paddingTop, start} = this.getScrollState(this.props);
+        if (paddingTop !== this.state.paddingTop
+        || paddingBottom !== this.state.paddingBottom
+        || end !== this.state.end
+        || start !== this.state.start) {
+            this.setState({
+                end: end,
+                paddingBottom: paddingBottom,
+                paddingTop: paddingTop,
+                start: start
+            });
+        }
+    }
+
+    playSong(i) {
+        const {playlist, dispatch} = this.props;
+        dispatch(playSong(playlist, i));
+    }
+
+    renderSongs(start, end) {
+        const chunk = 5;
+        const {authed, dispatch, playlist, playlists, playingSongId, songs, users} = this.props;
+        const items = playlist in playlists ? playlists[playlist].items : [];
+        let result = [];
+        for (let i = start; i < end; i += chunk) {
+            let songCards = items.slice(i, i + chunk).map((songId, j) => {
+                const song = songs[songId];
+                const user = users[song.user_id];
+                const index = i + j;
+                return (
+                    <div className='col-1-5' key={index + '-' + song.id}>
+                        <SongCard
+                            authed={authed}
+                            dispatch={dispatch}
+                            isActive={song.id === playingSongId}
+                            playSong={this.playSong.bind(this, index)}
+                            scrollFunc={fetchSongsIfNeeded.bind(null, playlist)}
+                            song={song}
+                            user={user} />
+                    </div>
+                );
+            }, this);
+
+            if (songCards.length < chunk) {
+                for (let j = 0; j < chunk - songCards.length + 1; j++) {
+                    songCards.push(<div className='col-1-5' key={'song-placeholder-' + (i + j)}></div>);
+                }
+            }
+
+            result.push(
+                <div className='songs-row grid' key={'songs-row-' + i}>{songCards}</div>
+            );
+        }
+
+        return result;
+    }
+
+    render() {
+        const {playlist, playlists} = this.props;
+        const {end, paddingBottom, paddingTop, start} = this.state;
+        const isFetching = playlist in playlists ? playlists[playlist].isFetching : false;
+
+
+        return (
+            <div className='content'>
+                <div className='padder' style={{height: paddingTop}}></div>
+                {this.renderSongs(start, end)}
+                <div className='padder' style={{height: paddingBottom}}></div>
+                {isFetching ? <Spinner /> : null}
+            </div>
+        );
+    }
+}
+
+export default InfiniteScrollify(SongCards);
