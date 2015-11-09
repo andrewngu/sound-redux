076980fc8ee54419a7a338d5669fd4cbca9645f5
Andrew Nguyen <andrewngu@gmail.com>
2015-11-08 19:08:22 -0800

    wiring up mobile player controls

--- scripts/actions/environment.js
+++ scripts/actions/environment.js
@@ -18,6 +18,10 @@ function changeWidthAndHeight(height, width) {
 export function initEnvironment() {
     return dispatch => {
         const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        if (isMobile) {
+            document.body.style.overflow = 'hidden';
+        }
+
         dispatch(changeIsMobile(isMobile));
         dispatch(changeWidthAndHeight(window.innerHeight, window.innerWidth));
 
--- scripts/components/MobilePlayer.js
+++ scripts/components/MobilePlayer.js
@@ -1,48 +1,15 @@
 import React, {Component, PropTypes} from 'react';
 import {VelocityComponent} from 'velocity-react';
-import {IMAGE_SIZES} from '../constants/SongConstants';
-import {formatSongTitle} from '../utils/FormatUtils';
-import {getImageUrl} from '../utils/SongUtils';
+import MobilePlayerContent from '../components/MobilePlayerContent';
 
 class MobilePlayer extends Component {
-    renderPlayer() {
-        const {playingSongId, songs, users} = this.props;
+    renderPlayerContent() {
+        const {playingSongId} = this.props;
         if (playingSongId === null) {
             return <div></div>;
         }
 
-        const song = songs[playingSongId];
-        const user = users[song.user_id];
-        const image = getImageUrl(song.artwork_url, IMAGE_SIZES.XLARGE);
-
-        return (
-            <div className='mobile-player' style={{backgroundImage: `url(${image})`}}>
-                <div className='mobile-player-bg'></div>
-                <div className='mobile-player-extras'></div>
-                <div className='mobile-player-content'>
-                    <div className='mobile-player-info'>
-                        <div className='mobile-player-title'>
-                            {formatSongTitle(song.title)}
-                        </div>
-                        <div className='mobile-player-user'>
-                            {user.username}
-                        </div>
-                    </div>
-                    <div className='mobile-player-controls'>
-                        <a className='mobile-player-button'>
-                            <i className='icon ion-ios-rewind'></i>
-                        </a>
-                        <a className='mobile-player-button'>
-                            <i className='ion-ios-play'></i>
-                        </a>
-                        <a className='mobile-player-button'>
-                            <i className='ion-ios-fastforward'></i>
-                        </a>
-                    </div>
-                </div>
-                <div className='mobile-player-seek-bar'></div>
-            </div>
-        );
+        return <MobilePlayerContent {...this.props} />;
     }
 
     render() {
@@ -50,7 +17,7 @@ class MobilePlayer extends Component {
         const isSongPlaying = playingSongId !== null;
         return (
             <VelocityComponent animation={{ height: isSongPlaying ? 100 : 0 }} duration={250}>
-                {this.renderPlayer()}
+                {this.renderPlayerContent()}
             </VelocityComponent>
         );
     }
