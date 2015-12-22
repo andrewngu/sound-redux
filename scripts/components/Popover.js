ec7ddca0b79b8a3972a0f2ea9980bbec8d98bcfe
John Mitchell <jlmitch5@ncsu.edu>
2015-12-21 22:23:26 -0500

    fixed popover toggle click to close bindings also fixed eslint stuff for popover file

--- scripts/components/Popover.js
+++ scripts/components/Popover.js
@@ -1,4 +1,4 @@
-import React, {Component, PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class Popover extends Component {
@@ -23,44 +23,62 @@ class Popover extends Component {
         document.removeEventListener('mousedown', this.onOutsideClick);
     }
 
-    handleOutsideClick() {
-        this.setState({
-            isOpen: false
-        });
-    }
-
     onOutsideClick(e) {
         if (!this.state.isOpen) {
             return;
         }
 
         e.stopPropagation();
+
         const localNode = ReactDOM.findDOMNode(this);
+        const source = e.target;
+
+        if (!localNode.contains(source)) {
+            this.handleOutsideClick();
+        }
+    }
+
+    handleOutsideClick() {
+        this.setState({
+            isOpen: false
+        });
+    }
+
+    toggleIsOpen(e) {
+        e.stopPropagation();
         let source = e.target;
 
-        while(source.parentNode) {
-            if (source === localNode) {
+        while (source.parentNode) {
+            if (source.getAttribute('class') &&
+                source.getAttribute('class').indexOf('handleToggle') > -1) {
+                this.setState({isOpen: !this.state.isOpen});
+                source = null;
                 return;
             }
             source = source.parentNode;
         }
-        this.handleOutsideClick();
-    }
-
-    toggleIsOpen() {
-        this.setState({isOpen: !this.state.isOpen});
     }
 
     render() {
         return (
             <div
                 className={this.props.className + ' popover' + (this.state.isOpen ? ' open' : '')}
-                onClick={this.toggleIsOpen}>
-                {this.props.children[0]}
-                {this.state.isOpen ? this.props.children[1] : null}
+                onClick={this.toggleIsOpen}
+            >
+                <div className='handleToggle'>
+                  {this.props.children[0]}
+                </div>
+                <div>
+                  {this.state.isOpen ? this.props.children[1] : null}
+                </div>
             </div>
-        )
+        );
     }
 }
+
+Popover.propTypes = {
+    children: PropTypes.any,
+    className: PropTypes.string
+};
 
 export default Popover;
