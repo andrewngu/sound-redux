b6346f84deed42eaaab5357c285266c1f500a059
Andrew Nguyen <andrewngu@gmail.com>
2016-02-18 07:33:12 -0800

    using import instead of require

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,9 @@ import fetch from 'isomorphic-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import '../styles/main.scss';
 import App from './containers/App';
 import configureStore from './store/configureStore';
-
-require('../styles/main.scss');
 
 const store = configureStore();
 
