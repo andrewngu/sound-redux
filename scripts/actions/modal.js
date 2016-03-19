0bedaced164f39cdeb5e85e2fd02ee588b391881
Andrew Nguyen <andrewngu@gmail.com>
2016-03-18 20:30:14 -0700

    fixing action lint errors

--- scripts/actions/modal.js
+++ scripts/actions/modal.js
@@ -1,8 +1,8 @@
 import * as types from '../constants/ActionTypes';
 
 export function changeModal(modal) {
-    return {
-        type: types.CHANGE_MODAL,
-        modal
-    };
+  return {
+    type: types.CHANGE_MODAL,
+    modal,
+  };
 }
