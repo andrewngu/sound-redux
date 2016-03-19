3698938e2cd016aecc620820c2fa65e6052a6c2b
Andrew Nguyen <andrewngu@gmail.com>
2016-03-19 02:07:47 -0700

    more mobile lint errors

--- scripts/components/MobileSongs.js
+++ scripts/components/MobileSongs.js
@@ -1,38 +1,49 @@
-import React, {Component, PropTypes} from 'react';
-import {fetchSongsIfNeeded} from '../actions/playlists';
+import React, { Component, PropTypes } from 'react';
+import { fetchSongsIfNeeded } from '../actions/playlists';
 import MobileSongList from '../components/MobileSongList';
 
+const propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  playingSongId: PropTypes.number,
+  playlist: PropTypes.object.isRequired,
+  playlists: PropTypes.object.isRequired,
+  songs: PropTypes.object.isRequired,
+  users: PropTypes.object.isRequired,
+};
+
 class MobileSongs extends Component {
-    componentWillMount() {
-        const {dispatch, playlist, playlists} = this.props;
-        if (!(playlist in playlists) || playlists[playlist].items.length === 0) {
-            dispatch(fetchSongsIfNeeded(playlist));
-        }
+  componentWillMount() {
+    const { dispatch, playlist, playlists } = this.props;
+    if (!(playlist in playlists) || playlists[playlist].items.length === 0) {
+      dispatch(fetchSongsIfNeeded(playlist));
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        const {dispatch, playlist, playlists} = this.props;
-        if (playlist !== nextProps.playlist) {
-            if (!(nextProps.playlist in playlists) || playlists[nextProps.playlist].items.length === 0) {
-                dispatch(fetchSongsIfNeeded(nextProps.playlist));
-            }
-        }
+  componentWillReceiveProps(nextProps) {
+    const { dispatch, playlist, playlists } = this.props;
+    if (playlist !== nextProps.playlist) {
+      if (!(nextProps.playlist in playlists) || playlists[nextProps.playlist].items.length === 0) {
+        dispatch(fetchSongsIfNeeded(nextProps.playlist));
+      }
     }
+  }
 
-    render() {
-        const {dispatch, playlist, playlists, songs, users, playingSongId} = this.props;
+  render() {
+    const { dispatch, playingSongId, playlist, playlists, songs, users } = this.props;
 
-        return (
-            <MobileSongList
-            playlist={playlist}
-            playlists={playlists}
-            songs={songs}
-            users={users}
-            playingSongId={playingSongId}
-            dispatch={dispatch}>
-            </MobileSongList>
-        );
-    }
+    return (
+      <MobileSongList
+        playlist={playlist}
+        playlists={playlists}
+        songs={songs}
+        users={users}
+        playingSongId={playingSongId}
+        dispatch={dispatch}
+      />
+    );
+  }
 }
+
+MobileSongs.propTypes = propTypes;
 
 export default MobileSongs;
