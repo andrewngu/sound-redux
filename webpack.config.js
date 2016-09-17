d032fb9e529bad35a6b545f16ed2d05d58ff29ec
Andrew Nguyen <andrewngu@gmail.com>
2016-09-17 12:48:50 -0700

    updating package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,29 @@ var webpack = require('webpack');
 var ignore = new webpack.IgnorePlugin(/\.svg$/)
 
 module.exports = {
-    devtool: 'source-map',
-    entry: {
-        main: [
-            './scripts/main.js',
-            'webpack-dev-server/client?http://localhost:8080',
-            'webpack/hot/only-dev-server'
-        ]
+  devtool: 'source-map',
+  entry: {
+    main: [
+      './scripts/main.js',
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+    ],
+  },
+  output: {
+    publicPath: 'http://localhost:8080/',
+    filename: '/js/[name].js',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['react-hot', 'babel?' + JSON.stringify({presets: ['react', 'es2015', 'stage-0']})], exclude: /node_modules/ },
+      { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
+    ],
+  },
+  plugins: [ignore],
+  devServer: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api/*': 'http://localhost:8081',
     },
-    output: {
-        publicPath: 'http://localhost:8080/',
-        filename: '/js/[name].js'
-    },
-    module: {
-        loaders: [
-          { test: /\.js$/, loaders: ['react-hot', 'babel?' + JSON.stringify({presets: ['react', 'es2015', 'stage-0']})], exclude: /node_modules/ },
-          { test: /\.scss$/, loaders: ['style', 'css', 'autoprefixer', 'sass'] }
-        ]
-    },
-    plugins: [ignore],
-    devServer: {
-        host: '0.0.0.0',
-        proxy: {
-            '/api/*': 'http://localhost:8081',
-        }
-    }
+  },
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,30 +5,30 @@ var nodeModulesDir = path.resolve(__dirname, 'node_modules');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: {
-        main: './scripts/main.js',
-        vendor: [
-            'lodash',
-            'moment',
-            'normalizr',
-            'react',
-            'redux',
-            'soundcloud'
-        ]
-    },
-    output: {
-        publicPath: 'http://localhost:8080/',
-        filename: './server/public/js/[name].js'
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel', exclude: [nodeModulesDir] },
-            { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!sass') }
-        ]
-    },
-    plugins: [
-        ignore,
-        new ExtractTextPlugin('./server/public/css/main.css'),
-        new webpack.optimize.CommonsChunkPlugin('vendor', './server/public/js/vendor.js')
-    ]
+  entry: {
+    main: './scripts/main.js',
+    vendor: [
+      'lodash',
+      'moment',
+      'normalizr',
+      'react',
+      'redux',
+      'soundcloud',
+    ],
+  },
+  output: {
+    publicPath: 'http://localhost:8080/',
+    filename: './server/public/js/[name].js',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel', exclude: [nodeModulesDir] },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!sass') },
+    ],
+  },
+  plugins: [
+    ignore,
+    new ExtractTextPlugin('./server/public/css/main.css'),
+    new webpack.optimize.CommonsChunkPlugin('vendor', './server/public/js/vendor.js'),
+  ],
 };
