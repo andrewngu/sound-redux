9b247de70c2b28fcc566a457cc23b47e1c062f48
Norbert de Langen <ndelangen@me.com>
2017-04-21 22:02:26 +0200

    CHANGE to use enzyme for rendering components in tests

--- test/components/Player.spec.js
+++ test/components/Player.spec.js
@@ -6,30 +6,44 @@ jest.mock('global', () => ({
   },
 }));
 
+const basicProps =  {
+  dispatch: () => {},
+  player: {},
+  playingSongId: 1,
+  playlists: {},
+  songs: { 1: { user_id: 1, title: '' } },
+  users: { 1: { username: ''} },
+};
+
+import React from 'React';
 import { localStorage } from 'global';
-import Player from '../../scripts/components/Player';
+import Player from '../../client/components/Player';
+import { mount } from 'enzyme';
 
 describe('volume', () => {
   it('initial level is 1', () => {
-    let player = new Player();
-    expect(player.state.volume).toEqual(1);
+    const props = { ...basicProps }
+    const player = mount(<Player {...props} />);
+    expect(player.state('volume')).toEqual(1);
   });
 
   it('can restore previous value from local storage', () => {
     localStorage.getItem.mockReturnValue(0.65)
-    let player = new Player();
-    expect(player.state.volume).toEqual(0.65);
+    const props = { ...basicProps }
+    const player = mount(<Player {...props} />);
+    expect(player.state('volume')).toEqual(0.65);
   });
 });
 
 describe('handleVolumeChange', () => {
   it('saves new value to localStorage', () => {
-    const player = new Player();
-    player.handleVolumeChange({
+    const props = { ...basicProps }
+    const player = mount(<Player {...props} />);
+    expect(player.instance().handleVolumeChange({
       currentTarget: {
         volume: 0.56
       }
-    });
+    }));
     expect(localStorage.setItem).toHaveBeenCalledWith('volume', 0.56);
   });
 });
