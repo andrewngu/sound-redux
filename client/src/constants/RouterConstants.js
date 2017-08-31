c4ba54b131f5260a6674f12ec0c96042c4ba8eb6
Andrew Nguyen <andrewngu@gmail.com>
2017-08-31 12:57:07 -0700

    adding RouterActions, RouterConstants, RouterUtils

---
+++ client/src/constants/RouterConstants.js
@@ -0,0 +1,6 @@
+export const SONGS_PATH = 'songs';
+export const SONG_PATH = 'songs/:id';
+export const USER_PATH = 'users/:id';
+export const STREAM_PATH = 'me/stream';
+export const LIKES_PATH = 'me/likes';
+export const PLAYLIST_PATH = 'me/playlists/:id';
