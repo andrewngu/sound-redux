7e29e8b9799bf983863f47e94b98d3b2e46c356b
Andrew Nguyen <andrewngu@gmail.com>
2017-09-01 14:50:20 -0700

    refactoring SongCards

--- client/src/components/SongCards.jsx
+++ client/src/components/SongCards.jsx
@@ -1,25 +1,30 @@
+/* global window */
+
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { playSong } from '../actions/PlayerActions';
 import infiniteScroll from '../components/infiniteScroll';
 import SongCard from '../components/SongCard';
 import Spinner from '../components/Spinner';
+import scrollState from '../utils/ScrollUtils';
+
+const defaultProps = {
+  playingSongId: null,
+};
 
 const propTypes = {
-  authed: PropTypes.object.isRequired,
+  authed: PropTypes.shape({}).isRequired,
   dispatch: PropTypes.func.isRequired,
-  height: PropTypes.number,
+  height: PropTypes.number.isRequired,
   playingSongId: PropTypes.number,
   playlist: PropTypes.string.isRequired,
-  playlists: PropTypes.object.isRequired,
+  playlists: PropTypes.shape({}).isRequired,
   songs: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  users: PropTypes.object.isRequired,
 };
 
 class SongCards extends Component {
   constructor(props) {
     super(props);
-    this.getScrollState = this.getScrollState.bind(this);
     this.onScroll = this.onScroll.bind(this);
 
     this.state = {
@@ -35,7 +40,9 @@ class SongCards extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const { end, paddingBottom, paddingTop, start } = this.getScrollState(nextProps);
+    const { height, songs } = nextProps;
+    const { end, paddingBottom, paddingTop, start } = scrollState(height, songs.length);
+
     if (paddingTop !== this.state.paddingTop
     || paddingBottom !== this.state.paddingBottom
     || start !== this.state.start
@@ -54,7 +61,8 @@ class SongCards extends Component {
   }
 
   onScroll() {
-    const { end, paddingBottom, paddingTop, start } = this.getScrollState(this.props);
+    const { height, songs } = this.props;
+    const { end, paddingBottom, paddingTop, start } = scrollState(height, songs.length);
     if (paddingTop !== this.state.paddingTop
     || paddingBottom !== this.state.paddingBottom
     || end !== this.state.end
@@ -68,45 +76,6 @@ class SongCards extends Component {
     }
   }
 
-  getScrollState(props) {
-    const { height, songs } = props;
-    const count = songs.length;
-
-    const MARGIN_TOP = 20;
-    const ROW_HEIGHT = 132;
-    const ITEMS_PER_ROW = 5;
-    const scrollY = window.scrollY;
-
-    let paddingTop = 0;
-    let paddingBottom = 0;
-    let start = 0;
-    let end = count;
-
-    if ((scrollY - ((ROW_HEIGHT * 3) + (MARGIN_TOP * 2))) > 0) {
-      const rowsToPad = Math.floor(
-        (scrollY - ((ROW_HEIGHT * 2) + (MARGIN_TOP))) / (ROW_HEIGHT + MARGIN_TOP)
-      );
-      paddingTop = rowsToPad * (ROW_HEIGHT + MARGIN_TOP);
-      start = rowsToPad * ITEMS_PER_ROW;
-    }
-
-    const rowsOnScreen = Math.ceil(height / (ROW_HEIGHT + MARGIN_TOP));
-    const itemsToShow = (rowsOnScreen + 5) * ITEMS_PER_ROW;
-    if (count > (start + itemsToShow)) {
-      end = start + itemsToShow;
-      const rowsToPad = Math.ceil((count - end) / ITEMS_PER_ROW);
-      paddingBottom = rowsToPad * (ROW_HEIGHT + MARGIN_TOP);
-    }
-
-    return {
-      end,
-      paddingBottom,
-      paddingTop,
-      start,
-    };
-  }
-
-
   playSong(i, e) {
     e.preventDefault();
     const { playlist, dispatch } = this.props;
@@ -115,12 +84,11 @@ class SongCards extends Component {
 
   renderSongs(start, end) {
     const chunk = 5;
-    const { authed, dispatch, playingSongId, songs, users } = this.props;
+    const { authed, dispatch, playingSongId, songs } = this.props;
     const result = [];
 
     for (let i = start; i < end; i += chunk) {
       const songCards = songs.slice(i, i + chunk).map((song, j) => {
-        const user = users[song.user_id];
         const index = i + j;
         const playSongFunc = this.playSong.bind(this, index);
 
@@ -132,14 +100,13 @@ class SongCards extends Component {
               isActive={song.id === playingSongId}
               playSong={playSongFunc}
               song={song}
-              user={user}
             />
           </div>
         );
       });
 
       if (songCards.length < chunk) {
-        for (let j = 0; j < chunk - songCards.length + 1; j++) {
+        for (let j = 0; j < chunk - (songCards.length + 1); j += 1) {
           songCards.push(<div className="col-1-5" key={`song-placeholder-${(i + j)}`} />);
         }
       }
@@ -170,6 +137,7 @@ class SongCards extends Component {
   }
 }
 
+SongCards.defaultProps = defaultProps;
 SongCards.propTypes = propTypes;
 
 export default infiniteScroll(SongCards);
