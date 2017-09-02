776cb311694a1aa195d332d976327e5569526d5d
Andrew Nguyen <andrewngu@gmail.com>
2017-09-01 22:19:44 -0700

    Player and player styles

---
+++ client/src/components/Player.jsx
@@ -0,0 +1,91 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Slider from '../components/Slider';
+import { formatSeconds } from '../utils/NumberUtils';
+
+const propTypes = {
+  player: PropTypes.shape({}).isRequired,
+  song: PropTypes.shape({}).isRequired,
+};
+
+const Player = ({ player, song }) => {
+  const { currentTime, duration, isPlaying, volume } = player;
+  const { artworkUrl, title, user } = song;
+  const { username } = user;
+
+  return (
+    <div className="player">
+      <div className="player__inner container">
+        <div className="player__section">
+          <div className="player__song">
+            <div className="player__song__artwork" style={{ backgroundImage: `url(${artworkUrl})` }} />
+            <div className="player__song__main">
+              <div className="player__song__title">
+                {title}
+              </div>
+              <div className="player__song__username">
+                {username}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="player__section">
+          <div className="player__buttons">
+            <div className="player__button">
+              <i className="player__button__icon ion-ios-rewind" />
+            </div>
+            <div className="player__button">
+              <i className={`player__button__icon ion-ios-${isPlaying ? 'pause' : 'play'}`} />
+            </div>
+            <div className="player__button">
+              <i className="player__button__icon ion-ios-fastforward" />
+            </div>
+          </div>
+        </div>
+        <div className="player__section player__section--flex">
+          <Slider
+            max={duration}
+            onChange={() => {}}
+            value={currentTime}
+          />
+        </div>
+        <div className="player__section">
+          <div className="player__time">
+            {formatSeconds(currentTime)}
+            <div className="player__time__separator">
+              /
+            </div>
+            {formatSeconds(duration)}
+          </div>
+        </div>
+        <div className="player__section">
+          <div className="player__buttons">
+            <div className="player__button">
+              <i className="player__button__icon ion-loop" />
+            </div>
+            <div className="player__button">
+              <i className="player__button__icon ion-shuffle" />
+            </div>
+            <div className="player__button">
+              <i className="player__button__icon ion-android-list" />
+            </div>
+            <div className="player__button">
+              <i className="player__button__icon ion-android-volume-mute" />
+            </div>
+          </div>
+        </div>
+        <div className="player__section player__section--volume">
+          <Slider
+            max={1}
+            onChange={() => {}}
+            value={volume}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Player.propTypes = propTypes;
+
+export default Player;
---
+++ client/src/utils/NumberUtils.js
@@ -0,0 +1,13 @@
+export const padZero = (num, size) => {
+  let s = String(num);
+  while (s.length < size) {
+    s = `0${s}`;
+  }
+  return s;
+};
+
+export const formatSeconds = (num) => {
+  const minutes = padZero(Math.floor(num / 60), 2);
+  const seconds = padZero(num % 60, 2);
+  return `${minutes}:${seconds}`;
+};
