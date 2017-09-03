8ab9f8e9bd389a6c3819566b4d0bd8130d7751a8
Andrew Nguyen <andrewngu@gmail.com>
2017-09-02 21:31:20 -0700

    fixing genre queries and adding titles to links

--- client/src/utils/PlaylistUtils.js
+++ client/src/utils/PlaylistUtils.js
@@ -1,13 +1,13 @@
 import moment from 'moment';
 import { CLIENT_ID } from '../constants/Config';
-import { GENRE_PLAYLIST_TYPE, SEARCH_PLAYLIST_TYPE } from '../constants/PlaylistConstants';
+import { GENRE_PLAYLIST_TYPE, GENRE_QUERY_MAP, SEARCH_PLAYLIST_TYPE } from '../constants/PlaylistConstants';
 
 const API_HOSTNAME = '//api.soundcloud.com';
 
 const genrePlaylistUrl = (playlist) => {
   const [genre, time] = playlist.split('|').slice(1);
 
-  const genreUriSegment = `&tags=${genre}`;
+  const genreUriSegment = `&tags=${GENRE_QUERY_MAP[genre] || genre}`;
   const timeUriSegment = time ? `&created_at[from]=${moment().subtract(Number(time), 'days').format('YYYY-MM-DD%2012:00:00')}` : '';
 
   return `${API_HOSTNAME}/tracks?linked_partitioning=1&client_id=${CLIENT_ID}&limit=50&offset=0${timeUriSegment}${genreUriSegment}`;
