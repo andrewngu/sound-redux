1dd5353b819588024c5842fca12d51c241d465ac
Andrew Nguyen <andrewngu@gmail.com>
2017-09-04 12:18:26 -0700

    allowing loader to take children

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,30 +2,32 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const defaultProps = {
+  children: null,
   className: '',
 };
 
 const propTypes = {
+  children: PropTypes.node,
   className: PropTypes.string,
   isLoading: PropTypes.bool.isRequired,
 };
 
-const Loader = ({ className, isLoading }) => {
-  if (!isLoading) {
-    return null;
+const Loader = ({ children, className, isLoading }) => {
+  if (isLoading) {
+    return (
+      <div className={`loader ${className}`}>
+        <div className="loader__rects">
+          <div className="loader__rect loader__rect--1" />
+          <div className="loader__rect loader__rect--2" />
+          <div className="loader__rect loader__rect--3" />
+          <div className="loader__rect loader__rect--4" />
+          <div className="loader__rect loader__rect--5" />
+        </div>
+      </div>
+    );
   }
 
-  return (
-    <div className={`loader ${className}`}>
-      <div className="loader__rects">
-        <div className="loader__rect loader__rect--1" />
-        <div className="loader__rect loader__rect--2" />
-        <div className="loader__rect loader__rect--3" />
-        <div className="loader__rect loader__rect--4" />
-        <div className="loader__rect loader__rect--5" />
-      </div>
-    </div>
-  );
+  return children;
 };
 
 Loader.defaultProps = defaultProps;
