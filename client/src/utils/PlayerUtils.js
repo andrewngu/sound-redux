b0d320fd280d8f4bb893a5754e976450e6d71d65
Andrew Nguyen <andrewngu@gmail.com>
2017-09-05 08:25:09 -0700

    styling volume button

--- client/src/utils/PlayerUtils.js
+++ client/src/utils/PlayerUtils.js
@@ -2,4 +2,14 @@ import { CLIENT_ID } from '../constants/Config';
 
 const prepareStreamUrl = s => `${s}?client_id=${CLIENT_ID}`;
 
+export const volumeClassName = (volume) => {
+  if (volume > 0.8) {
+    return 'ion-android-volume-up';
+  } else if (volume > 0) {
+    return 'ion-android-volume-down';
+  }
+
+  return '';
+};
+
 export default prepareStreamUrl;
