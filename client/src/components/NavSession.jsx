1d72a7e68fc832f5080a879b7cca6df37112bc32
Andrew Nguyen <andrewngu@gmail.com>
2017-09-07 02:06:24 -0700

    adding stream to nav

--- client/src/components/NavSession.jsx
+++ client/src/components/NavSession.jsx
@@ -7,15 +7,24 @@ const propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
   navigateTo: PropTypes.func.isRequired,
   showLikes: PropTypes.bool.isRequired,
+  showStream: PropTypes.bool.isRequired,
 };
 
-const NavSession = ({ isAuthenticated, navigateTo, showLikes }) => {
+const NavSession = ({ isAuthenticated, navigateTo, showLikes, showStream }) => {
   if (!isAuthenticated) {
     return null;
   }
 
   return (
     <div className="nav-session">
+      <Link
+        className={`nav-session__item ${showStream ? 'nav-session__item--active' : ''}`}
+        navigateTo={navigateTo}
+        path={SONGS_PATH}
+        options={{ s: 'stream' }}
+      >
+        Stream
+      </Link>
       <Link
         className={`nav-session__item ${showLikes ? 'nav-session__item--active' : ''}`}
         navigateTo={navigateTo}
