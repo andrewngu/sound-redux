921df3b53a331fa5ab416ae1a8cac58f2a3477d4
Andrew Nguyen <andrewngu@gmail.com>
2017-09-11 20:40:29 -0700

    making SongsHeaderGenres expandable

--- client/src/components/SongsHeaderGenres.jsx
+++ client/src/components/SongsHeaderGenres.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import Link from '../components/Link';
 import { SONGS_PATH } from '../constants/RouterConstants';
 
@@ -10,30 +10,55 @@ const propTypes = {
   time: PropTypes.string.isRequired,
 };
 
-const SongsHeaderGenres = ({ genre, genres, navigateTo, time }) => (
-  <div className="songs-header__genres">
-    <div className="songs-header__genres__active">
-      {genre || 'genre'}
-    </div>
-    <div className="songs-header__genres__main">
-      {genres.map(g => (
-        <div className="songs-header__genre__wrap" key={g.key}>
-          <Link
-            className={`songs-header__genre ${g.key === genre ? 'songs-header__genre--active' : ''}`}
-            navigateTo={navigateTo}
-            options={{
-              g: g.key,
-              ...time ? { t: time } : {},
-            }}
-            path={SONGS_PATH}
-          >
-            {g.key}
-          </Link>
+class SongsHeaderGenres extends Component {
+  constructor() {
+    super();
+    this.onClick = this.onClick.bind(this);
+    this.state = { expanded: false };
+  }
+
+  onClick() {
+    this.setState(state => ({
+      expanded: !state.expanded,
+    }));
+  }
+
+  render() {
+    const { expanded } = this.state;
+    const { genre, genres, navigateTo, time } = this.props;
+
+    return (
+      <div className={`songs-header__genres ${expanded ? 'songs-header__genres--expanded' : ''}`}>
+        <div
+          className="songs-header__genres__active"
+          onClick={this.onClick}
+          role="button"
+          tabIndex="0"
+        >
+          {genre || 'genre'}
         </div>
-      ))}
-    </div>
-  </div>
-);
+        <div className="songs-header__genres__main">
+          {genres.map(g => (
+            <div className={`songs-header__genre ${g.key === genre ? 'songs-header__genre--active' : ''}`}>
+              <Link
+                className="songs-header__genre__text"
+                navigateTo={navigateTo}
+                onClick={this.onClick}
+                options={{
+                  g: g.key,
+                  ...time ? { t: time } : {},
+                }}
+                path={SONGS_PATH}
+              >
+                {g.key}
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 SongsHeaderGenres.propTypes = propTypes;
 
